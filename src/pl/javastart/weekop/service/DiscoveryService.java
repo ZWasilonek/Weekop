a51bb17473caeb3fdf8ca94c5edf8f386cae7f1d
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-08-17 21:04:49 +0200

    DiscoveryService addDiscovery and createDiscovery methods added

--- src/pl/javastart/weekop/service/DiscoveryService.java
+++ src/pl/javastart/weekop/service/DiscoveryService.java
@@ -1,5 +1,31 @@
 package pl.javastart.weekop.service;
 
+import pl.javastart.weekop.dao.DiscoveryDAO;
+import pl.javastart.weekop.dao.factory.DAOFactory;
+import pl.javastart.weekop.model.Discovery;
+import pl.javastart.weekop.model.User;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class DiscoveryService {
+
+    public void addDiscovery(String name, String description, String url, User user) {
+        Discovery discovery = createDiscovery(name, description, url, user);
+        DAOFactory factory = DAOFactory.getDAOFactory();
+        DiscoveryDAO discoveryDAO = factory.getDiscoveryDAO();
+        discoveryDAO.create(discovery);
+    }
+
+    private Discovery createDiscovery(String name, String description, String url, User user) {
+        Discovery discovery = new Discovery();
+        discovery.setName(name);
+        discovery.setDescription(description);
+        discovery.setUrl(url);
+        User copyUser = new User(user);
+        discovery.setUser(copyUser);
+        discovery.setTimestamp(new Timestamp(new Date().getTime()));
+        return discovery;
+    }
 
 }
