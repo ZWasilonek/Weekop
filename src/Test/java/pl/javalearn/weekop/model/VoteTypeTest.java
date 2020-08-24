9b007551ac719f5365b2950aedd6f7e735642901
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-08-24 21:41:45 +0200

    VoteTypeTest started

---
+++ src/Test/java/pl/javalearn/weekop/model/VoteTypeTest.java
@@ -0,0 +1,13 @@
+package pl.javalearn.weekop.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VoteTypeTest implements ModelTest {
+
+    @Test
+    void givenVoteType_whenNotNull_thenCorrect() {
+
+    }
+}
