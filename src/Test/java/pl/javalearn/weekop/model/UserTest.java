567a898f18186f2514018db58f8024521346e7d5
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-08-24 21:34:34 +0200

    UserTest 2 tests finished

--- src/Test/java/pl/javalearn/weekop/model/UserTest.java
+++ src/Test/java/pl/javalearn/weekop/model/UserTest.java
@@ -1,20 +1,28 @@
 package pl.javalearn.weekop.model;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest implements ModelTest {
+
+    static private User user;
+
+    @BeforeAll
+    static void getInstance() {
+        user = new User();
+    }
     
     @Test
-    void givenUser_whenNotNull_thenCorrect() {
+    void givenUserProps_whenEqual_thenCorrect() {
         long id = 1;
         String username = "testUserName";
         String email = "test@email.com";
         String password = "passwordTest";
         boolean active = true;
 
-        User user = new User();
+        user = new User();
         user.setId(id);
         user.setUsername(username);
         user.setEmail(email);
@@ -27,6 +35,18 @@ class UserTest implements ModelTest {
                 () -> assertEquals(email, user.getEmail(), "Invalid EMAIL"),
                 () -> assertEquals(password, user.getPassword(), "Invalid PASSWORD"),
                 () -> assertEquals(active, user.isActive(), "Invalid active"));
+    }
+
+    @Test
+    void givenConstructorUser_whenPropsEqual_thenCorrect() {
+        User constructorUser = new User(user);
+        assertNotEquals(user, constructorUser, "Users are equals");
+
+        assertAll("Test constructor user props set",
+                () -> assertEquals(constructorUser.getId(), user.getId(), "Invalid ID"),
+                () -> assertEquals(constructorUser.getUsername(), user.getUsername(), "Invalid USERNAME"),
+                () -> assertEquals(constructorUser.getEmail(), user.getEmail(), "Invalid EMAIL"),
+                () -> assertEquals(constructorUser.getPassword(), user.getPassword(), "Invalid PASSWORD"));
     }
 
 }
