e6c6790abb47a2b815b3d90da3b92031bab20a8d
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-08-25 16:55:07 +0200

    VoteTypeTest cleaned

--- src/test/java/pl/javalearn/weekop/model/VoteTypeTest.java
+++ src/test/java/pl/javalearn/weekop/model/VoteTypeTest.java
@@ -1,10 +1,6 @@
 package pl.javalearn.weekop.model;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class VoteTypeTest implements ModelTest {
@@ -15,13 +11,6 @@ class VoteTypeTest implements ModelTest {
         String type = "VOTE_DOWN";
 
         assertEquals(type, voteType.toString());
-    }
-
-    @DisplayName("Enum Source Test")
-    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
-    @EnumSource(VoteType.class)
-    void enumTest(VoteType type) {
-        System.out.println(type);
     }
 
 }
